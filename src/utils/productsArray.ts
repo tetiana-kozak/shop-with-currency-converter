984b81ba883e562792099dd96701c62ff696dc99
Tetiana <kozaktanya077@gmail.com>
2023-04-01 16:31:58 +0300

    change prices

--- src/utils/productsArray.ts
+++ src/utils/productsArray.ts
@@ -10,37 +10,37 @@ const productsArray: Product[] = [
     id: 1,
     title: 'iPhone 14 Pro',
     description: 'This is iPhone 14 Pro',
-    price: 100,
+    price: 40000,
   },
   {
     id: 2,
     title: 'iPhone 13 Pro',
     description: 'This is iPhone 13 Pro',
-    price: 200,
+    price: 35000,
   },
   {
     id: 3,
     title: 'iPhone 12 Pro',
     description: 'This is iPhone 12 Pro',
-    price: 500,
+    price: 30000,
   },
   {
     id: 4,
     title: 'iPhone 10 Pro',
     description: 'This is iPhone 10 Pro',
-    price: 1000,
+    price: 25000,
   },
   {
     id: 5,
     title: 'iPhone 14 Pro',
     description: 'This is iPhone 14 Pro',
-    price: 1500,
+    price: 20000,
   },
   {
     id: 6,
     title: 'iPhone 8',
     description: 'This is iPhone 8 ',
-    price: 2000,
+    price: 15000,
   },
 ]
 
